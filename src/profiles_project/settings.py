3d2a9dd0f6de7df5a0b1dcd24d349889940afe12
hazem <hazem.salem.work@gmail.com>
2025-01-19 23:04:27 +0200

    Configured for deployment

--- src/profiles_project/settings.py
+++ src/profiles_project/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'django-insecure-feewdetz2z+15()(pn)rj@ws68s!l%)a+mp#q!2gv1x)80*w*i'
-)
+SECRET_KEY ='django-insecure-feewdetz2z+15()(pn)rj@ws68s!l%)a+mp#q!2gv1x)80*w*i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
