30a541554b195f86b10b1234273a3e576c050095
hazem <hazem.salem.work@gmail.com>
2025-01-19 23:02:22 +0200

    Configured for deployment

--- src/profiles_project/settings.py
+++ src/profiles_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-feewdetz2z+15()(pn)rj@ws68s!l%)a+mp#q!2gv1x)80*w*i'
-
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-feewdetz2z+15()(pn)rj@ws68s!l%)a+mp#q!2gv1x)80*w*i'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = ["*"]
 
@@ -122,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT ='static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
