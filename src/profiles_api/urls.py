a0b974cda04984681a9561c76a830ff774fb5ce8
hazem <hazem.salem.work@gmail.com>
2025-01-19 15:30:19 +0200

    Added ViewSets to profile_api

--- src/profiles_api/urls.py
+++ src/profiles_api/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path ,include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 
+VSF_router = DefaultRouter()
+VSF_router.register('viewset-features',views.ViewSetFeatures,basename='viewset-features')
 
 urlpatterns = [
-    path('apiview-features/',views.APIViewFeatures.as_view())
+    path('apiview-features/',views.APIViewFeatures.as_view()),
+    path('',include(VSF_router.urls)),
 ]
